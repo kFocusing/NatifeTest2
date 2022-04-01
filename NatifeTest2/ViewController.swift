7ec74ed3f42c979a016a11ca434c39c408dc2291
Danil Klimov <klymovd@gmail.com>
2022-04-01 16:28:27 +0300

    Initial Commit

---
+++ NatifeTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatifeTest2
+//
+//  Created by Danylo Klymov on 01.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
